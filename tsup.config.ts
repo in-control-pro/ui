fa81fda1a3af8fb23909ce03927e6c8c4ba6b632
Alejandro Jerez <alejandrojerez204@gmail.com>
2025-04-01 16:43:58 -0500

    styles: solved globals.css

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,10 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/components/ui/*.tsx'],
   format: ['esm', 'cjs'],
   dts: true,
   splitting: true,
@@ -9,4 +12,26 @@ export default defineConfig({
   clean: true,
   treeshake: true,
   external: ['react', 'react-dom'],
+  outDir: 'dist',
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : '.js',
+    };
+  },
+  loader: {
+    '.css': 'copy',
+  },
+  async onSuccess() {
+    await new Promise<void>((resolve) => {
+      const stylesDir = path.join(__dirname, 'dist/styles');
+      if (!fs.existsSync(stylesDir)) {
+        fs.mkdirSync(stylesDir, { recursive: true });
+      }
+      fs.copyFileSync(
+        path.join(__dirname, 'src/styles/globals.css'),
+        path.join(__dirname, 'dist/styles/globals.css')
+      );
+      resolve();
+    });
+  },
 });
